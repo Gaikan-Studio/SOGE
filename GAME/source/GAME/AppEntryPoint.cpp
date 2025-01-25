ac883bd95cb3dbeb28d37a11b1f666c3790439db
tuguzT <timurka.tugushev@gmail.com>
2025-01-25 04:52:14 +0300

    Add cartoon human mesh for utility AI showcase

--- GAME/source/GAME/AppEntryPoint.cpp
+++ GAME/source/GAME/AppEntryPoint.cpp
@@ -9,12 +9,8 @@
 #include <SOGE/Graphics/Deferred/DeferredRenderGraph.hpp>
 #include <SOGE/Graphics/Entities/AmbientLightEntity.hpp>
 #include <SOGE/Graphics/Entities/DirectionalLightEntity.hpp>
-#include <SOGE/Graphics/Entities/PointLightEntity.hpp>
 #include <SOGE/Graphics/Entities/StaticMeshEntity.hpp>
 #include <SOGE/Graphics/GraphicsModule.hpp>
-#include <SOGE/Graphics/Primitives/Box.hpp>
-#include <SOGE/Graphics/Primitives/Sphere.hpp>
-#include <SOGE/Graphics/Resources/SimpleTextureResource.hpp>
 #include <SOGE/Math/Camera.hpp>
 #include <SOGE/Window/WindowModule.hpp>
 
@@ -176,107 +172,22 @@ namespace soge_game
         auto& renderGraph = container.Provide<soge::DeferredRenderGraph>();
         graphicsModule->SetRenderGraph(renderGraph);
 
-        const auto [texture, textureUuid] =
-            graphicsModule->GetEntityManager().CreateEntity<soge::SimpleTextureResource>(
-                container.Provide<soge::GraphicsCore>(), "cardboard", "./resources/textures/cardboard.png");
-        SOGE_INFO_LOG(R"(Created texture with UUID {})", textureUuid.str());
-
-        constexpr std::size_t gridSize = 3;
-        constexpr glm::vec3 gridOffset{-0.5f * (gridSize - 1)};
-        for (std::size_t i = 0; i < gridSize; ++i)
-        {
-            for (std::size_t j = 0; j < gridSize; ++j)
-            {
-                for (std::size_t k = 0; k < gridSize; ++k)
-                {
-                    const auto [box, boxUuid] = graphicsModule->GetEntityManager().CreateEntity<soge::BoxPrimitive>(
-                        container.Provide<soge::BoxPrimitive>());
-                    SOGE_INFO_LOG(R"(Created box ({}, {}, {}) with UUID {})", i, j, k, boxUuid.str());
-
-                    const auto x = static_cast<float>(i);
-                    const auto y = static_cast<float>(j);
-                    const auto z = static_cast<float>(k);
-                    box.GetTransform() = soge::Transform{
-                        .m_position = glm::vec3{x, y, z} + gridOffset,
-                        // .m_rotation = glm::quat{glm::vec3{0.0f, glm::radians(45.0f), 0.0f}},
-                        .m_scale = glm::vec3{0.5f},
-                    };
-                    if ((i + j + k) % 2 == 0)
-                    {
-                        box.GetColorTexture() = texture.GetTextureResource();
-                    }
-                }
-            }
-        }
-
-        const auto [sphere, sphereUuid] = graphicsModule->GetEntityManager().CreateEntity<soge::SpherePrimitive>(
-            container.Provide<soge::SpherePrimitive>());
-        SOGE_INFO_LOG(R"(Created sphere with UUID {})", sphereUuid.str());
-        sphere.GetTransform() = soge::Transform{
-            .m_position = glm::vec3{2.0f, 0.0f, 0.0f},
-            .m_scale = glm::vec3{0.5f},
-        };
-
-        const auto [hog, hogUuid] = graphicsModule->GetEntityManager().CreateEntity<soge::StaticMeshEntity>(
+        const auto [human, humanUuid] = graphicsModule->GetEntityManager().CreateEntity<soge::StaticMeshEntity>(
             container.Provide<soge::StaticMeshEntity>());
-        SOGE_INFO_LOG(R"(Created hog with UUID {})", hogUuid.str());
-        hog.GetFilePath() = "./resources/meshes/hog.fbx";
-        hog.GetTransform() = soge::Transform{
-            .m_position = glm::vec3{0.0f, -1.0f, 4.0f},
+        SOGE_INFO_LOG(R"(Created human with UUID {})", humanUuid.str());
+        human.GetFilePath() = "./resources/meshes/cartoon-human.fbx";
+        human.GetTransform() = soge::Transform{
+            // .m_position = glm::vec3{0.0f, 0.0f, 10.0f},
             .m_rotation = glm::vec3{0.0f, glm::radians(180.0f), 0.0f},
-            .m_scale = glm::vec3{0.01f},
+            .m_scale = glm::vec3{0.001f},
         };
-        hog.Load();
-
-        // const auto [cake, cakeUuid] = graphicsModule->GetEntityManager().CreateEntity<soge::StaticMeshEntity>(
-        //     container.Provide<soge::StaticMeshEntity>());
-        // SOGE_INFO_LOG(R"(Created cake with UUID {})", cakeUuid.str());
-        // cake.GetFilePath() = "./resources/meshes/cake/cake.fbx";
-        // cake.GetTransform() = soge::Transform{
-        //     .m_position = glm::vec3{0.0f, -2.5f, 0.0f},
-        //     .m_rotation = glm::vec3{0.0f, glm::radians(180.0f), 0.0f},
-        //     .m_scale = glm::vec3{0.01f},
-        // };
-        // cake.Load();
-
-        // const auto [boat, boatUuid] = graphicsModule->GetEntityManager().CreateEntity<soge::StaticMeshEntity>(
-        //     container.Provide<soge::StaticMeshEntity>());
-        // SOGE_INFO_LOG(R"(Created boat with UUID {})", boatUuid.str());
-        // boat.GetFilePath() = "./resources/meshes/boat/boat.fbx";
-        // boat.GetTransform() = soge::Transform{
-        //     .m_position = glm::vec3{-2.5f, 0.0f, 0.0f},
-        //     .m_rotation = glm::vec3{glm::radians(90.0f), 0.0f, glm::radians(-90.0f)},
-        //     .m_scale = glm::vec3{0.005f},
-        // };
-        // boat.Load();
-
-        const auto [cheese, cheeseUuid] = graphicsModule->GetEntityManager().CreateEntity<soge::StaticMeshEntity>(
-            container.Provide<soge::StaticMeshEntity>());
-        SOGE_INFO_LOG(R"(Created cheese with UUID {})", cheeseUuid.str());
-        cheese.GetFilePath() = "./resources/meshes/cheese/cheese.fbx";
-        cheese.GetTransform() = soge::Transform{
-            .m_position = glm::vec3{0.0f, 0.0f, 7.0f},
-            // .m_rotation = glm::vec3{glm::radians(90.0f), 0.0f, glm::radians(-90.0f)},
-            .m_scale = glm::vec3{0.1f},
-        };
-        cheese.Load();
-
-        const auto [axe, axeUuid] = graphicsModule->GetEntityManager().CreateEntity<soge::StaticMeshEntity>(
-            container.Provide<soge::StaticMeshEntity>());
-        SOGE_INFO_LOG(R"(Created axe with UUID {})", axeUuid.str());
-        axe.GetFilePath() = "./resources/meshes/axe/axe.fbx";
-        axe.GetTransform() = soge::Transform{
-            .m_position = glm::vec3{0.0f, 0.0f, 10.0f},
-            .m_rotation = glm::vec3{0.0f, 0.0f, glm::radians(90.0f)},
-            .m_scale = glm::vec3{0.05f},
-        };
-        axe.Load();
+        human.Load();
 
         const auto [ambientLightEntity1, ambientLightEntityUuid1] =
             graphicsModule->GetEntityManager().CreateEntity<soge::AmbientLightEntity>(
                 container.Provide<soge::AmbientLightEntity>());
         SOGE_INFO_LOG(R"(Created ambient light entity with UUID {})", ambientLightEntityUuid1.str());
-        ambientLightEntity1.GetIntensity() = 0.01f;
+        ambientLightEntity1.GetIntensity() = 0.1f;
 
         const auto [directionalLightEntity1, directionalLightEntityUuid1] =
             graphicsModule->GetEntityManager().CreateEntity<soge::DirectionalLightEntity>(
@@ -286,13 +197,6 @@ namespace soge_game
             .m_rotation = glm::vec3{glm::radians(45.0f), glm::radians(45.0f), 0.0f},
         };
         directionalLightEntity1.GetDirection() = directionalLightTransform1.Forward();
-
-        const auto [pointLightEntity1, pointLightEntityUuid1] =
-            graphicsModule->GetEntityManager().CreateEntity<soge::PointLightEntity>(
-                container.Provide<soge::PointLightEntity>());
-        SOGE_INFO_LOG(R"(Created point light entity with UUID {})", pointLightEntityUuid1.str());
-        pointLightEntity1.GetAttenuation().m_linearFactor = glm::vec3{1.0f};
-        pointLightEntity1.GetAttenuation().m_quadraticFactor = glm::vec3{100.0f};
 
         const auto [camera, cameraUuid] = graphicsModule->GetCameraManager().CreateCamera({
             .m_width = static_cast<float>(window.GetWidth()),
@@ -376,7 +280,7 @@ namespace soge_game
 
         float cameraPitch{}, cameraYaw{};
         float lightPitch{glm::radians(45.0f)}, lightYaw{glm::radians(45.0f)};
-        constexpr float cameraSpeed = 1.0f;
+        constexpr float cameraSpeed = 1.5f;
         constexpr float cameraSensitivity = 0.005f;
         auto update = [=, &camera, &directionalLightEntity1](const soge::UpdateEvent& aEvent) mutable {
             {
